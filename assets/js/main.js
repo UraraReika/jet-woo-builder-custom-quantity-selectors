1abe7987f892c7dea2e3c831ec70fc821fdfe6f8
Alexandr Rudy <aoikomete@gmail.com>
2022-07-28 11:30:06 +0300

    FIX: Multiple cart types on same page

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,119 +30,123 @@
 		},
 
 		quantitySelector: function ( $scope ) {
-			let settings = JetWooBuilderCQS.getElementorElementSettings( $scope );
+			$scope.each( function () {
 
-			if ( settings && settings.enable_custom_quantity_selector ) {
-				let $quantityWrap = $scope.find( '.quantity:not(.jet-woo-quantity-button-added)' ),
-					increaseControl = settings.selected_quantity_increase_button_icon.value,
-					decreaseControl = settings.selected_quantity_decrease_button_icon.value,
-					controlsPosition = settings.quantity_buttons_position,
-					increaseHtml = '',
-					decreaseHtml = '';
+				let settings = JetWooBuilderCQS.getElementorElementSettings( $( this ) );
 
-				if ( 'object' === typeof increaseControl ) {
-					increaseHtml = '<img class="icon-svg" src="' + increaseControl.url + '" alt="increase icon">';
-				} else {
-					increaseHtml = '<i class="' + increaseControl + '"></i>';
-				}
+				if ( settings && settings.enable_custom_quantity_selector ) {
+					let $quantityWrap = $( this ).find( '.quantity:not(.jet-woo-quantity-button-added)' ),
+						increaseControl = settings.selected_quantity_increase_button_icon.value,
+						decreaseControl = settings.selected_quantity_decrease_button_icon.value,
+						controlsPosition = settings.quantity_buttons_position,
+						increaseHtml = '',
+						decreaseHtml = '';
 
-				if ( 'object' === typeof decreaseControl ) {
-					decreaseHtml = '<img class="icon-svg" src="' + decreaseControl.url + '" alt="decrease icon">';
-				} else {
-					decreaseHtml = '<i class="' + decreaseControl + '"></i>';
-				}
-
-				$quantityWrap.each( function () {
-					let $quantityBox = $( this ).find( '.qty' );
-
-					if ( $quantityBox && 'date' !== $quantityBox.attr( 'type' ) && 'hidden' !== $quantityBox.attr( 'type' ) ) {
-						let $quantityParent = $quantityBox.parent();
-
-						$quantityParent.addClass( 'jet-woo-quantity-button-added position-' + controlsPosition );
-
-						switch ( controlsPosition ) {
-							case 'horizontal':
-							case 'vertical':
-								$quantityParent.prepend( '<a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a>' );
-								$quantityParent.append( '<a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a>' );
-
-								break;
-							case 'start':
-							case 'top':
-								$quantityParent.prepend( '<div class="jet-woo-qty-controls-holder"><a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a><a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a></div>');
-								break;
-							case 'end':
-							case 'bottom':
-								$quantityParent.append( '<div class="jet-woo-qty-controls-holder"><a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a><a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a></div>');
-								break;
-						}
-
-						$( this ).find( '.icon-svg' ).each( function() {
-							let img = $( this ),
-								image_uri   = img.attr( 'src' );
-
-							$.get( image_uri, function( data ) {
-								let svg = $( data ).find( 'svg' );
-
-								svg.removeAttr( 'xmlns:a' );
-								img.replaceWith( svg );
-							}, 'xml' );
-						} );
-
-						let $min = $quantityBox.attr( 'min' );
-
-						if ( $min && $min > 0 && parseFloat( $quantityBox.val() ) < $min ) {
-							$quantityBox.val( $min );
-						}
-
-						let increaseDecreaseObject = $( this ).find( '.jet-woo-qty-control' );
-
-						increaseDecreaseObject.each( function () {
-							$( this ).on( 'click', function( e ) {
-								e.preventDefault();
-
-								let $currentQuantityValue = $quantityBox.val(),
-									$maxQuantity = $quantityBox.attr( 'max' ),
-									$minQuantity = $quantityBox.attr( 'min' ),
-									$step = $quantityBox.attr( 'step' );
-
-								if ( ! $currentQuantityValue || '' === $currentQuantityValue || 'NaN' === $currentQuantityValue ) {
-									$currentQuantityValue = 0;
-								}
-
-								if ( '' === $maxQuantity || 'NaN' === $maxQuantity ) {
-									$maxQuantity = '';
-								}
-
-								if ( '' === $minQuantity || 'NaN' === $minQuantity ) {
-									$minQuantity = 0;
-								}
-
-								if ( 'any' === $step || '' === $step || undefined === $step || 'NaN' === $step ) {
-									$step = 1;
-								}
-
-								if ( $( e.target ).parent().hasClass( 'increase' ) || $( e.target ).hasClass( 'increase' ) ) {
-									if ( +$maxQuantity && ( +$maxQuantity === +$currentQuantityValue || +$currentQuantityValue > +$maxQuantity ) ) {
-										$quantityBox.val( $maxQuantity );
-									} else {
-										$quantityBox.val( parseFloat( $currentQuantityValue ) + parseFloat( $step ) );
-									}
-								} else {
-									if ( +$minQuantity && ( +$minQuantity === +$currentQuantityValue || +$currentQuantityValue < +$minQuantity ) ) {
-										$quantityBox.val( $minQuantity );
-									} else if ( $currentQuantityValue > 0 ) {
-										$quantityBox.val( parseFloat( $currentQuantityValue ) - parseFloat( $step ) );
-									}
-								}
-
-								// Trigger change event.
-								$quantityBox.trigger( 'change' );
-							} );
-						} );
+					if ( 'object' === typeof increaseControl ) {
+						increaseHtml = '<img class="icon-svg" src="' + increaseControl.url + '" alt="increase icon">';
+					} else {
+						increaseHtml = '<i class="' + increaseControl + '"></i>';
 					}
-				} );
-			}
+
+					if ( 'object' === typeof decreaseControl ) {
+						decreaseHtml = '<img class="icon-svg" src="' + decreaseControl.url + '" alt="decrease icon">';
+					} else {
+						decreaseHtml = '<i class="' + decreaseControl + '"></i>';
+					}
+
+					$quantityWrap.each( function () {
+						let $quantityBox = $( this ).find( '.qty' );
+
+						if ( $quantityBox && 'date' !== $quantityBox.attr( 'type' ) && 'hidden' !== $quantityBox.attr( 'type' ) ) {
+							let $quantityParent = $quantityBox.parent();
+
+							$quantityParent.addClass( 'jet-woo-quantity-button-added position-' + controlsPosition );
+
+							switch ( controlsPosition ) {
+								case 'horizontal':
+								case 'vertical':
+									$quantityParent.prepend( '<a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a>' );
+									$quantityParent.append( '<a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a>' );
+
+									break;
+								case 'start':
+								case 'top':
+									$quantityParent.prepend( '<div class="jet-woo-qty-controls-holder"><a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a><a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a></div>');
+									break;
+								case 'end':
+								case 'bottom':
+									$quantityParent.append( '<div class="jet-woo-qty-controls-holder"><a href="javascript:void(0)" class="jet-woo-qty-control increase">' + increaseHtml + '</a><a href="javascript:void(0)" class="jet-woo-qty-control decrease">' + decreaseHtml + '</a></div>');
+									break;
+							}
+
+							$( this ).find( '.icon-svg' ).each( function() {
+								let img = $( this ),
+									image_uri   = img.attr( 'src' );
+
+								$.get( image_uri, function( data ) {
+									let svg = $( data ).find( 'svg' );
+
+									svg.removeAttr( 'xmlns:a' );
+									img.replaceWith( svg );
+								}, 'xml' );
+							} );
+
+							let $min = $quantityBox.attr( 'min' );
+
+							if ( $min && $min > 0 && parseFloat( $quantityBox.val() ) < $min ) {
+								$quantityBox.val( $min );
+							}
+
+							let increaseDecreaseObject = $( this ).find( '.jet-woo-qty-control' );
+
+							increaseDecreaseObject.each( function () {
+								$( this ).on( 'click', function( e ) {
+									e.preventDefault();
+
+									let $currentQuantityValue = $quantityBox.val(),
+										$maxQuantity = $quantityBox.attr( 'max' ),
+										$minQuantity = $quantityBox.attr( 'min' ),
+										$step = $quantityBox.attr( 'step' );
+
+									if ( ! $currentQuantityValue || '' === $currentQuantityValue || 'NaN' === $currentQuantityValue ) {
+										$currentQuantityValue = 0;
+									}
+
+									if ( '' === $maxQuantity || 'NaN' === $maxQuantity ) {
+										$maxQuantity = '';
+									}
+
+									if ( '' === $minQuantity || 'NaN' === $minQuantity ) {
+										$minQuantity = 0;
+									}
+
+									if ( 'any' === $step || '' === $step || undefined === $step || 'NaN' === $step ) {
+										$step = 1;
+									}
+
+									if ( $( e.target ).parent().hasClass( 'increase' ) || $( e.target ).hasClass( 'increase' ) ) {
+										if ( +$maxQuantity && ( +$maxQuantity === +$currentQuantityValue || +$currentQuantityValue > +$maxQuantity ) ) {
+											$quantityBox.val( $maxQuantity );
+										} else {
+											$quantityBox.val( parseFloat( $currentQuantityValue ) + parseFloat( $step ) );
+										}
+									} else {
+										if ( +$minQuantity && ( +$minQuantity === +$currentQuantityValue || +$currentQuantityValue < +$minQuantity ) ) {
+											$quantityBox.val( $minQuantity );
+										} else if ( $currentQuantityValue > 0 ) {
+											$quantityBox.val( parseFloat( $currentQuantityValue ) - parseFloat( $step ) );
+										}
+									}
+
+									// Trigger change event.
+									$quantityBox.trigger( 'change' );
+								} );
+							} );
+						}
+					} );
+				}
+
+			} );
 		},
 
 		getElementorElementSettings: function( $scope ) {
